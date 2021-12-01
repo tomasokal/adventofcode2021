bc137c7fcedabeb1a49a2f3177934999daad6892
Tomas Okal <tomas.okal@gmail.com>
2021-11-30 23:19:50 -0600

    Day 1 Solution

--- Day 1/solution.R
+++ Day 1/solution.R
@@ -1,9 +1,23 @@
+# Libraries
 library(data.table)
 
+# Load data
 input <- data.table::fread("Day 1/input.csv"
                            , header = FALSE
                            )
 
 # Part 1
+output = data.frame(original = as.numeric(input$V1))
+output$shift = shift(output$original, -1)
+output$check = ifelse(output$shift > output$original, 1, 0)
+
+solution = sum(output$check, na.rm = TRUE)
 
 # Part 2
+output = data.table(original = as.numeric(input$V1))
+output$rollsum = frollsum(output[, original], 3)
+output <- na.omit(output)
+output$rollsum_shift = shift(output$rollsum, -1)
+output$check = ifelse(output$rollsum_shift > output$rollsum, 1, 0)
+
+solution = sum(output$check, na.rm = TRUE)
