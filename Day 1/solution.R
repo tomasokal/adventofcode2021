37c73da541cd6519328ef36fbb34889bd009d291
Tomas Okal <tomas.okal@gmail.com>
2021-11-30 23:30:39 -0600

    Update to loading data.

--- Day 1/solution.R
+++ Day 1/solution.R
@@ -2,19 +2,18 @@
 library(data.table)
 
 # Load data
-input <- data.table::fread("Day 1/input.csv"
-                           , header = FALSE
-                           )
+input <- readLines("Day 1/input.txt") |>
+  as.numeric()
 
 # Part 1
-output = data.frame(original = as.numeric(input$V1))
+output = data.frame(original = input)
 output$shift = shift(output$original, -1)
 output$check = ifelse(output$shift > output$original, 1, 0)
 
 solution = sum(output$check, na.rm = TRUE)
 
 # Part 2
-output = data.table(original = as.numeric(input$V1))
+output = data.table(original = input)
 output$rollsum = frollsum(output[, original], 3)
 output <- na.omit(output)
 output$rollsum_shift = shift(output$rollsum, -1)
